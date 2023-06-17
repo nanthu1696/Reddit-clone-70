f44e41f3654e38604c91792383875cf4d3785946
Nanthakumar Sathiyanandham <118247818+nanthu1696@users.noreply.github.com>
2023-06-17 16:19:51 +0530

    Create Logo.js

---
+++ src/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <div className="headContainer">
+      <div></div>
+      <div></div>
+      <button id="logo"></button>
+    </div>
+  );
+}
+
+export default Logo;
