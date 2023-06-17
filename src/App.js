4c24a54b65ab58054d5a48b12040c59352faf409
Nanthakumar Sathiyanandham <118247818+nanthu1696@users.noreply.github.com>
2023-06-17 16:18:58 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import Logo from "./Logo";
+
+export default function App() {
+  return (
+    <div className="appBody">
+      <header>
+        <Logo />
+      </header>
+    </div>
+  );
+}
